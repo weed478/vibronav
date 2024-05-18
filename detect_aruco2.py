3391c99c3c9f9540fab45ec9d9827280f4b73b79
Ewa <ewapelc00@gmail.com>
2024-05-18 10:50:38 +0200

    change object point generation

--- detect_aruco2.py
+++ detect_aruco2.py
@@ -4,42 +4,41 @@ import numpy as np
 import argparse
 
 
-# def generate_grid_obj_points(rows, cols, code_size, spacing, heights):
-#     points = []
-#     for row in range(rows):
-#         for col in range(cols):
-#             x0 = col * (code_size + spacing)
-#             x1 = x0 + code_size
-#             y0 = row * (code_size + spacing)
-#             y1 = y0 + code_size
-
-#             z0, z1, z2, z3 = heights[row * cols + col]
-
-#             points.append([x0, y0, z0])
-#             points.append([x1, y0, z1])
-#             points.append([x1, y1, z2])
-#             points.append([x0, y1, z3])
-#     return np.array(points, dtype=np.float32)
-
-
-def generate_ring_obj_points(radius, num_codes, code_size, heights):
-    circumference = 2 * np.pi * radius
-    code_angle = code_size / circumference * 2 * np.pi
+def generate_plus_obj_points(code_size, plate_height, spacing):
     points = []
-    for i in range(num_codes):
-        angle0 = 2 * np.pi * i / num_codes
-        angle1 = angle0 + code_angle
 
+    # positions of top-lef corners of markers in shape "+"
+    positions_topleft = [
+        (0, 0),                         # central marker
+        ((code_size + spacing*2), 0),     # right marker
+        (0, (code_size + spacing*2)),     # top marker
+        (-(code_size + spacing*2), 0),    # left marker
+        (0, -(code_size + spacing*2))     # bottom marker
+    ]
+
+    # heights of corners on a plate
+    h = plate_height
+    heights = [
+        (h, h, h, h),
+        (h, 0, 0, h),
+        (0, 0, h, h),
+        (0, h, h, 0),
+        (h, h, 0, 0)
+    ]
+
+    # generate corner coordinates for each marker
+    for i, (x, y) in enumerate(positions_topleft):
         z0, z1, z2, z3 = heights[i]
 
-        points.append([[radius * np.cos(angle0), radius * np.sin(angle0), z0],
-                       [radius * np.cos(angle1), radius * np.sin(angle1), z1],
-                       [radius * np.cos(angle1), radius * np.sin(angle1), z2],
-                       [radius * np.cos(angle0), radius * np.sin(angle0), z3]])
+        points.append([[x, y, z0],
+                       [x + code_size, y, z1],
+                       [x + code_size, y - code_size, z2],
+                       [x, y - code_size, z3]])
+
     return np.array(points, dtype=np.float32)
 
 
-def main(a, h):
+def main(a, h, s):
     cap = cv2.VideoCapture(1)
     dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
     detector_params = aruco.DetectorParameters()
@@ -51,18 +50,7 @@ def main(a, h):
 
     dist_coeffs = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
 
-    circumference = 225
-
-    heights = [
-        (h, h, h, h),
-        (h, 0, 0, h),
-        (0, 0, h, h),
-        (0, h, h, 0),
-        (h, h, 0, 0)
-    ]
-
-
-    all_obj_points = generate_ring_obj_points(circumference / (2 * np.pi), 5, a, heights)
+    all_obj_points = generate_plus_obj_points(a, h, s)
 
     while True:
         ret, frame = cap.read()
@@ -105,5 +93,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Arguments to main')
     parser.add_argument('a', type=int, help='Code size')
     parser.add_argument('h', type=float, help='Plate height')
+    parser.add_argument('s', type=float, help='Spacing between aruco markers')
     args = parser.parse_args()
-    main(args.a, args.h)
+    main(args.a, args.h, args.s)
